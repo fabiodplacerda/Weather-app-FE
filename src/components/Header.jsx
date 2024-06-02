36e53320198dc7d96599bbd6273901dff1bbba5b
fabiodplacerda <lacerda96@gmail.com>
2024-06-02 21:59:33 +0100

    feat: Add Sign Up and Login links to the header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,8 +22,18 @@ export const Header = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
+                <a className="nav-link" href="#">
                   Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Login
                 </a>
               </li>
               <li className="nav-item dropdown">
