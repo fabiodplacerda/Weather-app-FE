fa620fd0ee6aeeb93ed7b5935a34e23043f8156b
fabiodplacerda <lacerda96@gmail.com>
2024-06-02 21:55:49 +0100

    CHORE: Update footer styling to include fixed positioning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   return (
     <div
       id="footer"
-      className="d-flex justify-content-center align-items-center"
+      className="d-flex justify-content-center align-items-center fixed-bottom"
     >
       <p className="text-center">&copy; {date.getFullYear()} DFCorp</p>
     </div>
