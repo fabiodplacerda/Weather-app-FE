7cb238a1fff4518d1e32e91e3e5457be655565d6
fabiodplacerda <lacerda96@gmail.com>
2024-06-02 21:53:12 +0100

    CHORE: Add main container component and update background image styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { ContainerMain } from "./components/ContainerMain";
 
 const App = () => {
   return (
     <>
       <Header />
+      <ContainerMain />
       <Footer />
     </>
   );
