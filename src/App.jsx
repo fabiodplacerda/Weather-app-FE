33c701d9f685a5c9bc7ab688dc52e6cc78fdc5ff
fabiodplacerda <lacerda96@gmail.com>
2024-06-03 19:37:40 +0100

    NEW: Added routing in App component and add SignUp and SearchForm components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { Footer } from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+
 import { Header } from "./components/Header";
-import { ContainerMain } from "./components/ContainerMain";
+import { SignUp } from "./components/SignUp";
+import { SearchForm } from "./components/SearchForm";
+import { Footer } from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
-      <ContainerMain />
+      <main className="mt-5 mb-5 align-items-center d-flex" id="main-container">
+        <Routes>
+          <Route path="/" element={<SearchForm />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -9,6 +11,8 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
