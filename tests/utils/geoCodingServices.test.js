96c2e6ea1f2ae25cfcdd900237853e46637c090d
fabiodplacerda <lacerda96@gmail.com>
2024-06-04 19:46:43 +0100

    new: created geoCoding services and associated tests

---
+++ tests/utils/geoCodingServices.test.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+import { getCities } from "../../src/utils/geocoding.service";
+import testCities from "../testData/testCities";
+import { expect } from "vitest";
+
+vi.mock("axios");
+
+describe("Geo Code data services tests", () => {
+  const mockedResolvedCitiesData = { data: testCities };
+  it("should make the correct data call", async () => {
+    // Arrange
+    axios.get.mockResolvedValueOnce(mockedResolvedCitiesData);
+    // Act
+    await getCities();
+    // Assert
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/results");
+  });
+
+  it("should return the correct data", async () => {
+    // Arrange
+    axios.get.mockResolvedValueOnce(mockedResolvedCitiesData);
+    // Act
+    const result = await getCities();
+    // Assert
+    expect(result).toEqual(testCities);
+  });
+
+  it("should return an error message if request fails", async () => {
+    // Arrange
+    const error = { message: "Error" };
+    axios.get.mockRejectedValueOnce(error);
+    // Act
+    const result = await getCities();
+    // Assert
+    expect(result).toBe(error);
+  });
+});
