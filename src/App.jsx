ff1d20bde73f5dcbf0464f3d463b1d0dfc361d4a
fabiodplacerda <lacerda96@gmail.com>
2024-06-07 18:43:48 +0100

    chore: Update SearchForm component to pass selected city to Weather component

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,26 @@ import { SearchForm } from "./components/SearchForm";
 import { Footer } from "./components/Footer";
 import { Weather } from "./components/Weather";
 import { Login } from "./components/Login";
+import { useState } from "react";
 
 const App = () => {
+  const [selectedCity, setSelectedCity] = useState({});
+
   return (
     <>
       <Header />
       <main className="mt-5 mb-5 align-items-center d-flex" id="main-container">
         <Routes>
-          <Route path="/" element={<SearchForm />} />
+          <Route
+            path="/"
+            element={<SearchForm setSelectedCity={setSelectedCity} />}
+          />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/weather" element={<Weather />} />
+          <Route
+            path="/weather/:id"
+            element={<Weather selectedCity={selectedCity} />}
+          />
         </Routes>
       </main>
       <Footer />
